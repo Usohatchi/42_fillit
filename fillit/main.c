baa66b4ee9427b53491815ae3b2b1546602d15e9
happynger <happynger@gmail.com>
2018-10-10 20:15:10 -0700

    ?

--- fillit/main.c
+++ fillit/main.c
@@ -6,7 +6,7 @@
 /*   By: otahirov <otahirov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/09 19:30:00 by otahirov          #+#    #+#             */
-/*   Updated: 2018/10/10 19:26:55 by otahirov         ###   ########.fr       */
+/*   Updated: 2018/10/10 20:14:55 by otahirov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,6 @@ int		main(int ac, char **av)
 		error();
 	piece = ft_mapcheck(av[1]);
 	board = fillit_solve(piece);
-	printf("%li\n", board->board[0]);
-	printf("%li\n", board->board[1]);
-	printf("%li\n", board->board[2]);
-	printf("%li\n", board->board[3]);
-	printf("%i", board->bmin);
+	fillit_print(piece, board);	
 	return (0);
 }
